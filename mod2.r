9aae5b096750b388c3271f3f0079331f4a83eee4
Daniel Kzrysztof Mansur Pedraza <138751591+KzrysztMansur@users.noreply.github.com>
2024-05-04 21:43:23 -0600

    Update mod2.r
    unos cambios de ultimo momento del código

--- mod2.r
+++ mod2.r
@@ -1,5 +1,6 @@
 # Vectores
 # Crear un vector
+cat("\033[1m=== VECTOR ===\033[0m\n")
 mi_vector <- c(1, 2, 3, 4, 5)
 print(mi_vector)
 
@@ -12,6 +13,7 @@ print(resultado)
 
 # Arrays
 # Crear un array
+cat("\033[1m=== ARRAYS ===\033[0m\n")
 mi_array <- array(1:12, dim = c(2, 3, 2))
 print(mi_array)
 
@@ -25,6 +27,7 @@ print(resultado_array)
 
 # Matrices
 # Crear una matriz
+cat("\033[1m=== MATRICES ===\033[0m\n")
 mi_matriz <- matrix(1:9, nrow = 3, ncol = 3)
 print(mi_matriz)
 
@@ -39,6 +42,7 @@ print(resultado_matriz)
 
 # Data Frames
 # Crear un data frame
+cat("\033[1m=== DATA FRAMES ===\033[0m\n")
 mi_data_frame <- data.frame(
 Nombre = c("Juan", "María", "Pedro"),
   Edad = c(25, 30, 28),
@@ -53,7 +57,3 @@ print(mi_data_frame[2, "Edad"])
 # Operaciones con data frames
 mi_data_frame$Edad <- mi_data_frame$Edad + 1
 print(mi_data_frame)
-
-
-
-
